93a4ff2a0d512ae866980ecd76b640b20371bc8a
Eloiza <eloiza.souza@zup.com.br>
2024-12-04 13:26:52 -0300

    feat: add Scanner and principal menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+    boolean running = true;
+        System.out.println("* * * C A L C U L A D O R A * * *");
+        while (running) {
+
+            System.out.println("""
+                    Digite o número da operação desejada:
+                    1 - soma
+                    2 - subtração
+                    3 - multiplicação
+                    4 - divisão
+                    5 - exponenciação
+                    6 - raiz
+                    7 - percentual
+                    8 - sair""");
+            System.out.print("Opção: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 8){
+                running = false;
+                System.out.println("Calculadora encerrada.");
+            }
+        }
+        scanner.close();
     }
 }
